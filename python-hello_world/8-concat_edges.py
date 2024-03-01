4a163cdfe09da5a79495ea8c0ba6d2f31ebe267b
Hirw23 <w.hirwa@alustudent.com>
2024-03-01 04:36:56 -0800

    done

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-tr = "Python is an interpreted, interactive, object-oriented programming\
+str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+str = str[39:66] + str[106:112] + str[:6]
+print(str)#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = str[39:66] + str[106:112] + str[:6]
 print(str)
