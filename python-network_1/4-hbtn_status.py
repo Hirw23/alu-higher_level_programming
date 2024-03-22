f7a56f303085d91a9c6d9fbbbd5e627981dafcc8
Hirw23 <w.hirwa@alustudent.com>
2024-03-22 16:40:23 -0700

    fetched

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -5,7 +5,7 @@ import requests
 
 
 if __name__ == "__main__":
-    response = requests.get("https://intranet.hbtn.io/status")
+    message = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
-    print("\t- type: {}".format(type(response.text)))
-    print("\t- content: {}".format(response.text))
+    print("\t- type: {}".format(type(message.text)))
+    print("\t- content: {}".format(message.text))
