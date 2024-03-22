16c4b4d61e43b2fb8f860782f4a40165da81bd22
Hirw23 <w.hirwa@alustudent.com>
2024-03-22 06:37:45 -0700

    done

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 # curl to send request to the url
-
-if [ -z "$1" ]; then
-  echo "Please provide a URL as an argument"
-  exit 1
-fi
-echo "Size of the response body in bytes: $response"
+curl -sl "$1" |grep 'content-length'| cut -d " " -f2 
