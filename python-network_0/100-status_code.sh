3ddf89db98c73465485407d9e2dba8eac51627ca
Hirw23 <w.hirwa@alustudent.com>
2024-03-23 17:21:55 -0700

    hopee

--- python-network_0/100-status_code.sh
+++ python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # send a request to a url passed as an argument,and displays only the status code of the response
-curl -so /dev/null --write-out "%{http_code}" "$1"
+curl -s -o /dev/null -w "%{http_code}" "$1"
