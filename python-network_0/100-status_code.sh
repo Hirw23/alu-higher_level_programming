feeedc27f6cd6d25d7ace06f817a5b6b2f328369
Hirw23 <w.hirwa@alustudent.com>
2024-04-01 04:06:20 -0700

    body

--- python-network_0/100-status_code.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# send a request to a url passed as an argument,and displays only the status code of the response
-curl -s -o /dev/null -w "%{http_code}" "$1"
--- python-network_0/101-post_json.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# sends a json post request to a rul passsed as the first argument,a dn displays the body of the response
-curl -sL -H "content-type:application/json"  -d @"$2" -X POST "$1"
