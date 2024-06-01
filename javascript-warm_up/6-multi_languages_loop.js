4dacd7b19ad27920b8a0de1de683824b5c4bc7c0
Hirw23 <w.hirwa@alustudent.com>
2024-06-01 03:31:51 -0700

    multi

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
+/* print 3 lines using the loop */
 const array = ['C is fun', 'Phython is cool', 'Javascript is amazing'];
 for (let i = 0; i < array.length; i++) {
   console.log(array[i]);
