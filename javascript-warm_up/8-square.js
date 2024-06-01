12df758ca466db25a70b129dcab95a7a4962a770
Hirw23 <w.hirwa@alustudent.com>
2024-06-01 03:45:27 -0700

    square

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -6,8 +6,8 @@ if (size === undefined || isNaN(parseInt(size))) {
   for (let i = 0; i < parseInt(size); i++) {
     let row = '';
     for (let j = 0; j < parseInt(size); j++) {
-       row  += 'X';
-  }
-   console.log(row);
+      row += 'X';
+    }
+    console.log(row);
   }
 }
