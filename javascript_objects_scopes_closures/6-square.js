99b48292ec62e9e54c4d2fe7aa72ad42880bfc73
Hirw23 <w.hirwa@alustudent.com>
2024-06-07 20:23:03 -0700

    ssquare

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,13 +1,15 @@
 #!/usr/bin/node
 const supSquare = require('./5-square.js');
+
 class Square extends supSquare {
   charprint (C) {
     if (C == null) {
-      C = 'X'; 
+      C = 'X';
     }
     for (let i = 0; i < this.width; i++) {
       console.log(C.repeat(this.width));
     }
   }
 }
+
 module.exports = Square;
